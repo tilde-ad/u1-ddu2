b1b786e3c750a14dfccecf4e6e225e3b14003e20
tilde-ad <tilde.davidsen@gmail.com>
2024-11-18 14:45:44 +0100

    Länkar och deklarationer
    Länkar till CSS-filer och script, samt deklarerat och tilldelat några konstanter till existerande html-element, även en variabel för prompt för "vilken stad?".

--- U1.distances/website/index.js
+++ U1.distances/website/index.js
@@ -4,3 +4,7 @@
 
 // Recommended: Ask for the city name and then the rest of the code
 
+let whatCity = prompt("Vilken stad?");
+const main = document.querySelector("main");
+const title = document.querySelector("title");
+const cityName = document.querySelector("h2");
