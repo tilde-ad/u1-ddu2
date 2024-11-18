deded7f39cca220f4bb79f78e874fb43f8a8e1e3
tilde-ad <tilde.davidsen@gmail.com>
2024-11-18 15:29:52 +0100

    Påbörjat kod för tabell
    Påbörjat kod för tabell med länder och avstånd. Inte färdig, och kommer återkomma till vid senare tillfälle.

--- U1.distances/website/index.js
+++ U1.distances/website/index.js
@@ -8,3 +8,19 @@ let whatCity = prompt("Vilken stad?");
 const main = document.querySelector("main");
 const title = document.querySelector("title");
 const cityName = document.querySelector("h2");
+const table = document.querySelector("#table");
+
+for (i = 0; i <= 38; i++) {
+    let div = document.createElement("div");
+    table.append(div);
+    div.setAttribute("class", "head_column")
+    div.setAttribute("class", "cell")
+    div.textContent = cities[i].id + "-" + cities[i].name;
+}
+for (i = 0; i <= 38; i++) {
+    let div = document.createElement("div");
+    table.append(div);
+    div.setAttribute("class", "head_row")
+    div.setAttribute("class", "cell")
+    div.textContent = cities[i].id;
+}
