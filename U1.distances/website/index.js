b9564b97223ff9ef2421c515e5f34ecf5e8dbd1a
tilde-ad <tilde.davidsen@gmail.com>
2024-11-18 16:41:21 +0100

    Påbörjat "cities"-del
    Har påbörjat del med cities - skapat divarna, lagt in städerna och applicerat existerande CSS samt kod för att ändra bakgrund- och förgrundsfärg när staden skrivs in i prompt

--- U1.distances/website/index.js
+++ U1.distances/website/index.js
@@ -9,7 +9,16 @@ const main = document.querySelector("main");
 const title = document.querySelector("title");
 const cityName = document.querySelector("h2");
 const table = document.querySelector("#table");
+const city = document.querySelector("#cities");
+const info = document.querySelector("h3");
 
+for (i = 0; i <= 38; i++) {
+    let div = document.createElement("div");
+    city.append(div);
+    div.setAttribute("class", "cityBox")
+    div.setAttribute("id", i);
+    div.textContent = cities[i].name;
+}
 for (i = 0; i <= 38; i++) {
     let div = document.createElement("div");
     table.append(div);
@@ -23,4 +32,11 @@ for (i = 0; i <= 38; i++) {
     div.setAttribute("class", "head_row")
     div.setAttribute("class", "cell")
     div.textContent = cities[i].id;
+}
+
+for (i = 0; i <= 38; i++) {
+    if (whatCity == cities[i].name) {
+        document.getElementById(i).style.backgroundColor = "black";
+        document.getElementById(i).style.color = "white"
+    }
 }
